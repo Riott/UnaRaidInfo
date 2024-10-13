50a0beb5626c274b0e92b93f666eee2539452208
Cai Jones <1808209+Riott@users.noreply.github.com>
2024-10-13 16:48:59 +0100

    Updated raid images 2024-10-13 15:48:59.5424144 +0000 UTC

--- UnaRaids.lua
+++ UnaRaids.lua
@@ -1,4 +1,3 @@
-#this file is autogen from a go binary don't fuck with it
 una_raids = {
     ["1832318"] = {["date"] = "2024-10-14", ["start"] = "19:45", ["end"] = "23:00", ["picture"] = "1832318.png"}
 
