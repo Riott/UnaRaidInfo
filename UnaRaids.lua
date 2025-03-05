485aff99b6d05323dbc3eff36e1a3c6c1367471a
Steve Server <opc@instance-20221209-1246.subnet12091248.vcn12091248.oraclevcn.com>
2025-03-05 23:00:05 +0000

    Updated raid info: 1741215605

--- UnaRaids.lua
+++ UnaRaids.lua
@@ -1,5 +1,5 @@
 una_raids = {
-    ["1995727"] = {["date"] = "2025-03-05", ["start"] = "19:45", ["end"] = "23:00", ["picture"] = "1995727.png", ["bosses"] = {
+    ["1995728"] = {["date"] = "2025-03-10", ["start"] = "19:45", ["end"] = "23:00", ["picture"] = "1995728.png", ["bosses"] = {
 ["Vexie and the Geargrinders"] = {["roster"] = {["tanks"] = {}, ["healers"] = {}, ["melee"] = {}, ["ranged"] = {} } },
 ["Cauldron of Carnage"] = {["roster"] = {["tanks"] = {}, ["healers"] = {}, ["melee"] = {}, ["ranged"] = {} } },
 ["Rik Reverb"] = {["roster"] = {["tanks"] = {}, ["healers"] = {}, ["melee"] = {}, ["ranged"] = {} } },
